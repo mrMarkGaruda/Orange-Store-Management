00b343316b8af78a2e84ee317f5080862bad5db5
Mark Abramenko <mark.abramenko@epita.fr>
2025-02-27 18:17:59 +0100

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/epita/orangestoremanagement/OrangeStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.epita.orangestoremanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeStoreManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeStoreManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epita/orangestoremanagement/OrangeStoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epita.orangestoremanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeStoreManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
